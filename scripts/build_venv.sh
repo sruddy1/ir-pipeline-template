b4100e6ed4c4f1955498750301206494fdbcf3d6
Sean <sruddy1@udayton.edu>
2025-12-17 12:37:41 -0500

    added build_venv.sh

--- scripts/build_venv.sh
+++ scripts/build_venv.sh
@@ -10,12 +10,6 @@ python -m pip install --upgrade pip
 
 python -m pip install -r requirements.txt
 
-python ./scripts/init_repo.py
-
 python -m pip install -e .
 
 python -m ipykernel install --user --name="$REPO_NAME"
-
-git add -A
-git commit -m "repo initialization"
-git push
